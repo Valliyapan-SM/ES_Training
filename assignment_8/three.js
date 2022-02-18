4c3635191a8562a9f4a61a81b40374438fd2d0c9
Valliyapan-SM <valliyapan.sm@rently.com>
2022-02-18 12:22:46 +0530

    Completed assignment 6 and 8

---
+++ assignment_8/three.js
@@ -0,0 +1,11 @@
+var my_array = [];
+my_array.push("I am a String");
+my_array.push(3.14);
+my_array.push(['a','e','i','o','u']);
+var my_object = {
+    fname: "Valliyapan",
+    lname: "SM",
+    age: 21
+};
+my_array.push(my_object);
+console.log(my_array)
---
+++ assignment_8/two.js
@@ -0,0 +1,14 @@
+//splice method
+const days = ["Sunday","Monday","Wednesday","Delete me from array","Thursday","Delete me from array"]
+//deleting element from array
+days.splice(3,1);
+console.log("After deleting an element..");
+console.log(days);
+//insert Tuesday using splice
+console.log("After inserting Tuesday..");
+days.splice(2,0,"Tuesday");
+console.log(days);
+//delete element and add elements at same time
+days.splice(5,1,"Friday","Saturday");
+console.log("After deleting and adding elements at same time");
+console.log(days);
