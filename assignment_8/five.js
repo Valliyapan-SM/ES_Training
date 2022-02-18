3ffe6f2a3bbb1980f3f81495a01359bb081f4250
Valliyapan SM <99707756+Valliyapan-SM@users.noreply.github.com>
2022-02-18 12:55:09 +0530

    Completed assignments 6 and 8

---
+++ assignment_8/five.js
@@ -0,0 +1,13 @@
+var person = {
+    fname: "Valliyapan",
+    lname: "SM",
+    age: 21,
+    fav_colour: ['blue','pink'],
+    grades: {
+        DS: "A+",
+        CPP: "S",
+        M1: "A",
+        DSD: "S"
+    }
+};
+console.log(JSON.stringify(person));
